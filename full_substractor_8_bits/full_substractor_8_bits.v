b6ea0beb286d45ba421b3b828483619dd01030fd
eacerdas <antonio14.cr@gmail.com>
2023-05-08 19:09:15 -0600

    indentation correction

--- full_substractor_8_bits/full_substractor_8_bits.v
+++ full_substractor_8_bits/full_substractor_8_bits.v
@@ -37,39 +37,36 @@ module estimulo_1_bit;
 endmodule
 
 module r_c_8_bit_subtractor(x, y, z, d, b);
-  // Inputs
-  input [7:0] x, y;
-  input z;
-  // Outputs
-  output [7:0] d;
-  output b;
+    // Inputs
+    input [7:0] x, y;
+    input z;
+    // Outputs
+    output [7:0] d;
+    output b;
+    // Internal Nets
+    wire [7:0] d_int;
+    wire [6:0] b_int;
 
-  // Internal Nets
-  wire [7:0] d_int;
-  wire [6:0] b_int;
+    // 1-bit subtractor instances
+    r_c_1_bit subtractor_0(x[0], y[0],        z, d_int[0], b_int[0]);
+    r_c_1_bit subtractor_1(x[1], y[1], b_int[0], d_int[1], b_int[1]);
+    r_c_1_bit subtractor_2(x[2], y[2], b_int[1], d_int[2], b_int[2]);
+    r_c_1_bit subtractor_3(x[3], y[3], b_int[2], d_int[3], b_int[3]);
+    r_c_1_bit subtractor_4(x[4], y[4], b_int[3], d_int[4], b_int[4]);
+    r_c_1_bit subtractor_5(x[5], y[5], b_int[4], d_int[5], b_int[5]);
+    r_c_1_bit subtractor_6(x[6], y[6], b_int[5], d_int[6], b_int[6]);
+    r_c_1_bit subtractor_7(x[7], y[7], b_int[6], d_int[7], b);
 
-  // 1-bit subtractor instances
-  r_c_1_bit subtractor_0(x[0], y[0],        z, d_int[0], b_int[0]);
-  r_c_1_bit subtractor_1(x[1], y[1], b_int[0], d_int[1], b_int[1]);
-  r_c_1_bit subtractor_2(x[2], y[2], b_int[1], d_int[2], b_int[2]);
-  r_c_1_bit subtractor_3(x[3], y[3], b_int[2], d_int[3], b_int[3]);
-  r_c_1_bit subtractor_4(x[4], y[4], b_int[3], d_int[4], b_int[4]);
-  r_c_1_bit subtractor_5(x[5], y[5], b_int[4], d_int[5], b_int[5]);
-  r_c_1_bit subtractor_6(x[6], y[6], b_int[5], d_int[6], b_int[6]);
-  r_c_1_bit subtractor_7(x[7], y[7], b_int[6], d_int[7], b);
-
-  // Output connections
-  assign d = d_int;
-  assign b = b_int[6];
+    // Output connections
+    assign d = d_int;
+    assign b = b_int[6];
 
 endmodule
 
 module estimulo_8_bit;
-  
     // Inputs
     reg [7:0] X, Y;
     reg Z;
-    
     // Outputs
     wire [7:0] D;
     wire B;
