aa44c390dd11b0e2539cbf68c0cd407a379820dc
eacerdas <antonio14.cr@gmail.com>
2023-05-10 20:24:13 -0600

    Now it displays a tkinter GUI to select the file you want to open if there is more than one

--- compile_script.py
+++ compile_script.py
@@ -2,6 +2,108 @@ import argparse
 import os
 import shutil
 import subprocess
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import ttk 
+
+
+def count_files():
+    count = 0
+    for root, dirs, files in os.walk(".", topdown=False):
+        for file in files:
+            if file.endswith(".v"):
+                count += 1
+    return count
+
+def get_verilog_file():
+    verilog_file = None
+    count = 0
+    for root, dirs, files in os.walk(".", topdown=False):
+        for file in files:
+            if file.endswith(".v"):
+                count += 1
+                if verilog_file is None:
+                    verilog_file = file
+                else:
+                    verilog_file = None  # hay más de un archivo .v, no devolvemos nada
+                    break
+        if verilog_file is None and count > 1:
+            break  # hay más de un archivo .v, no seguimos buscando
+    return verilog_file
+
+def process_filename(filename):
+    # Obtener el nombre base del archivo
+    basename = os.path.basename(filename)
+    
+    # Verificar si el nombre ya está recortado
+    if basename.endswith('.v'):
+        return basename
+    else:
+        # Recortar el nombre y retornar
+        return basename.split('\\')[-1]
+
+class FileSelector(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        style = ttk.Style()
+        style.theme_use('clam')
+
+        self.file_label = tk.Label(self, text="Select the file you want to compile:")
+        self.file_label.pack(side="left")
+
+        self.file_menu = tk.OptionMenu(self, tk.StringVar(), "")
+        self.file_menu.config(width=40, padx=5)
+        self.file_menu.pack(side="left")
+
+        self.file_menu['menu'].config(bg='#E0E0E0', fg='black')
+        self.file_menu['menu'].config(activebackground='#0078D7', activeforeground='white')
+
+        self.quit_button = ttk.Button(self, text="Open", command=self.master.destroy)
+        self.quit_button.pack(side="right")
+
+        self.update_file_list()
+
+    def update_file_list(self):
+        self.file_menu['menu'].delete(0, 'end')
+        for root, dirs, files in os.walk("."):
+            for v_file in files:
+                if v_file.endswith('.v'):
+                    self.file_menu['menu'].add_command(label=os.path.join(root, v_file), command=lambda v=os.path.join(root, v_file): self.set_file(v))
+
+    def set_file(self, v_file):
+        self.file_menu['text'] = v_file
+        self.selected_file = v_file
+
+
+# Parse command line arguments
+#parser = argparse.ArgumentParser(description="Compiles verilog")
+#parser.add_argument("filename", help="name of the file to copy")
+#args = parser.parse_args()
+
+num_verilog_files = count_files()
+
+
+print("DEBUG: Número de archivos .v en la carpeta actual y subdirectorios:", count_files())
+
+# Display a tkinter window if there is more than 1 .v file
+if num_verilog_files > 1:
+    root = tk.Tk()
+    root.title("Verilog file manager")
+    file_selector = FileSelector(root)
+    root.mainloop()
+
+    verilog_file_ubication = file_selector.selected_file
+    print(f"The selected file is: {verilog_file_ubication}")
+else: 
+    #verilog_file_ubication = args.filename
+    verilog_file_ubication = get_verilog_file()
+
+verilog_file_name = process_filename(verilog_file_ubication)
 
 # Set the original directory
 original_directory = os.getcwd()
@@ -15,13 +117,8 @@ gtkwave_bin_directory = "C:/iverilog/gtkwave/bin"
 
 ##### MOVING .v TO iverilog\bin #####
 
-# Parse command line arguments
-parser = argparse.ArgumentParser(description="Copy a verilog file to the iverilog bin directory.")
-parser.add_argument("filename", help="name of the file to copy")
-args = parser.parse_args()
-
 # Get the full path to the file
-file_path = os.path.abspath(args.filename)
+file_path = os.path.abspath(verilog_file_ubication)
 
 # Check if file exists
 if not os.path.isfile(file_path):
@@ -32,9 +129,9 @@ if not os.path.isfile(file_path):
 
 # Check if file already exists in destination directory
 overwrite_flag = 0
-if os.path.isfile(os.path.join(verilog_bin_directory, args.filename)):
+if os.path.isfile(os.path.join(verilog_bin_directory, verilog_file_name)):
     # If it exists, delete it first to overwrite it
-    os.remove(os.path.join(verilog_bin_directory, args.filename))
+    os.remove(os.path.join(verilog_bin_directory, verilog_file_name))
     overwrite_flag = 1
 
 # Copy the file to the destination directory
@@ -42,9 +139,9 @@ shutil.copy(file_path, verilog_bin_directory)
 
 # Print success message
 if overwrite_flag == 0:
-    print(f"Copied \"{args.filename}\" to {verilog_bin_directory} in order to compile it. \n")
+    print(f"Copied \"{verilog_file_name}\" to {verilog_bin_directory} in order to compile it. \n")
 else:
-    print(f"Copied (overwritten) \"{args.filename}\" on {verilog_bin_directory} in order to compile it. \n")
+    print(f"Copied (overwritten) \"{verilog_file_name}\" on {verilog_bin_directory} in order to compile it. \n")
 
 
 ##### COMPILING VERILOG #####
@@ -53,10 +150,10 @@ else:
 os.chdir(verilog_bin_directory)
 
 # Eliminates de .v extension to the name to use it as output name
-filename_without_ext = os.path.splitext(args.filename)[0]
+filename_without_ext = os.path.splitext(verilog_file_name)[0]
 
 # Compile the Verilog code
-iverilog_command = f"iverilog.exe -o {filename_without_ext} {args.filename}"
+iverilog_command = f"iverilog.exe -o {filename_without_ext} {verilog_file_name}"
 print(f"Compiling Verilog code with command: {iverilog_command}")
 if subprocess.call(iverilog_command, shell=True) != 0:
     print("Error: Failed to compile Verilog code.")
@@ -88,10 +185,10 @@ output_file_path = os.path.join(verilog_bin_directory, filename_without_ext)
 if os.path.isfile(output_file_path):
     os.remove(output_file_path)
 
-file_path = os.path.join(verilog_bin_directory, args.filename)
+file_path = os.path.join(verilog_bin_directory, verilog_file_name)
 if os.path.isfile(file_path):
     os.remove(file_path)
-print(f"Removed \"{args.filename}\" from {verilog_bin_directory}. It is no longer needed.")
+print(f"Removed \"{verilog_file_name}\" from {verilog_bin_directory}. It is no longer needed.")
 
 # Find the generated VCD file
 vcd_file = None
